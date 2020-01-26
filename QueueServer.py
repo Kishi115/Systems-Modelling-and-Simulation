c61bd42983e1266f1eed686b1e12fc4cc42d73d7
Kishi115 <theythose@gmail.com>
2020-01-26 06:04:56 +0500

    Update QueueServer.py

--- QueueServer.py
+++ QueueServer.py
@@ -1,7 +1,10 @@
 import random
 from tabulate import tabulate
 
+# no. of arrivals
 n = 100000
+
+# column headers
 interval = [random.randint(0, 10)]
 arriveTime = [1]
 waitTime = [0]
@@ -9,6 +12,8 @@ timeAtServe = [1]
 serveTime = [random.randint(1, 5)]
 departTime = [timeAtServe[0] + serveTime[0]]
 queueLine = [0]
+
+# average wait and departure times, and average queue length
 avW = [0]
 avD = [departTime[0]]
 avQ = [0]
@@ -27,6 +32,9 @@ for x in range(1, n):
     serveTime.append(random.randint(1, 5))
     departTime.append(timeAtServe[x] + serveTime[x])
 
+    
+    # minus index of first person who departed after current arrival
+    # from index of current arrival
     for y in range(0, x + 1):
         if(departTime[y] > arriveTime[x]):
             queueLine.append(x - y)
@@ -39,7 +47,7 @@ for x in range(1, n):
     avQ.append(sumQ/x)
     
       
-  
+  # formatting into ascii table
 for x in range(n):
     table.append([interval[x], arriveTime[x], waitTime[x], timeAtServe[x], serveTime[x], departTime[x], queueLine[x], avW[x], avD[x], avQ[x]])
 
